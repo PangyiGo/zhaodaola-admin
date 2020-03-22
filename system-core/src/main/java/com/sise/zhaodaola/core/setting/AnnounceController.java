7310028e17aceee6c94275fa003f60a6703f424e
tinypy <1306359812@qq.com>
2020-03-22 23:59:52 +0800

    网站公告

---
+++ system-core/src/main/java/com/sise/zhaodaola/core/setting/AnnounceController.java
@@ -0,0 +1,72 @@
+package com.sise.zhaodaola.core.setting;
+
+import com.sise.zhaodaola.business.entity.Announce;
+import com.sise.zhaodaola.business.service.AnnounceService;
+import com.sise.zhaodaola.business.service.dto.BasicQueryDto;
+import com.sise.zhaodaola.business.service.dto.PageQueryCriteria;
+import com.sise.zhaodaola.tool.annotation.Log;
+import com.sise.zhaodaola.tool.utils.PageHelper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @Author: PangYi
+ * @Date 2020/3/2210:29 下午
+ */
+@RestController
+@RequestMapping("/api/announce")
+@Slf4j
+public class AnnounceController {
+
+    private AnnounceService announceService;
+
+    public AnnounceController(AnnounceService announceService) {
+        this.announceService = announceService;
+    }
+
+    @Log("新增网站公告")
+    @PreAuthorize("@auth.check('announce:create')")
+    @RequestMapping("/create")
+    public ResponseEntity<Object> createAnnounce(@RequestBody Announce announce) {
+        announceService.createAnnounce(announce);
+        return new ResponseEntity<>("新增成功", HttpStatus.OK);
+    }
+
+    @Log("删除网站公告")
+    @PreAuthorize("@auth.check('announce:deelte')")
+    @RequestMapping("/delete")
+    public ResponseEntity<Object> deleteAnnounce(@RequestBody List<Integer> announceIds) {
+        announceService.deleteAnnounce(announceIds);
+        return new ResponseEntity<>("删除成功", HttpStatus.OK);
+    }
+
+    @Log("修改网站公告")
+    @PreAuthorize("@auth.check('announce:update')")
+    @RequestMapping("/update")
+    public ResponseEntity<Object> updateAnnounce(@RequestBody Announce announce) {
+        announceService.updateAnnonce(announce);
+        return new ResponseEntity<>("修改成功", HttpStatus.OK);
+    }
+
+    @Log("查询网站公告")
+    @PreAuthorize("@auth.check('announce:list')")
+    @RequestMapping("/list")
+    public ResponseEntity<Object> getAnnounceList(BasicQueryDto basicQueryDto, PageQueryCriteria queryCriteria) {
+        PageHelper announceList = announceService.getAnnounceList(basicQueryDto, queryCriteria);
+        return ResponseEntity.ok(announceList);
+    }
+
+    @RequestMapping("/getOne/{announceId}")
+    public ResponseEntity<Object> getOne(@PathVariable("announceId") Integer announceId) {
+        Announce announce = announceService.getOne(announceId);
+        return ResponseEntity.ok(announce);
+    }
+}
