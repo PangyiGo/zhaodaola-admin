3ffd96521df18a056e43d4471d5e023180089e32
tinypy <1306359812@qq.com>
2020-03-22 23:59:34 +0800

    网站公告

--- system-business/src/main/java/com/sise/zhaodaola/business/service/AnnounceService.java
+++ system-business/src/main/java/com/sise/zhaodaola/business/service/AnnounceService.java
@@ -2,10 +2,53 @@ package com.sise.zhaodaola.business.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.sise.zhaodaola.business.entity.Announce;
+import com.sise.zhaodaola.business.service.dto.BasicQueryDto;
+import com.sise.zhaodaola.business.service.dto.PageQueryCriteria;
+import com.sise.zhaodaola.tool.utils.PageHelper;
+
+import java.util.List;
 
 /**
  * @Author: PangYi
  * @Date 2020/3/610:46 下午
  */
 public interface AnnounceService extends IService<Announce> {
+
+    /**
+     * 创建网站公告
+     *
+     * @param announce /
+     */
+    void createAnnounce(Announce announce);
+
+    /**
+     * 删除系统公告
+     *
+     * @param announceIds /
+     */
+    void deleteAnnounce(List<Integer> announceIds);
+
+    /**
+     * 修改系统公告
+     *
+     * @param announce /
+     */
+    void updateAnnonce(Announce announce);
+
+    /**
+     * 分页查询系统公告列表
+     *
+     * @param basicQueryDto /
+     * @param queryCriteria /
+     * @return /
+     */
+    PageHelper getAnnounceList(BasicQueryDto basicQueryDto, PageQueryCriteria queryCriteria);
+
+    /**
+     * 获取公告
+     *
+     * @param announceId /
+     * @return /
+     */
+    Announce getOne(Integer announceId);
 }
--- system-business/src/main/java/com/sise/zhaodaola/business/service/impl/AnnouceServiceImpl.java
+++ system-business/src/main/java/com/sise/zhaodaola/business/service/impl/AnnouceServiceImpl.java
@@ -1,12 +1,25 @@
 package com.sise.zhaodaola.business.service.impl;
 
+import cn.hutool.core.date.DatePattern;
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.ObjectUtils;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.sise.zhaodaola.business.entity.Announce;
+import com.sise.zhaodaola.business.entity.User;
 import com.sise.zhaodaola.business.mapper.AnnounceMapper;
 import com.sise.zhaodaola.business.service.AnnounceService;
+import com.sise.zhaodaola.business.service.dto.BasicQueryDto;
+import com.sise.zhaodaola.business.service.dto.PageQueryCriteria;
+import com.sise.zhaodaola.tool.utils.*;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * @Author: PangYi
@@ -15,4 +28,53 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @Transactional(propagation = Propagation.SUPPORTS, readOnly = true, rollbackFor = Exception.class)
 public class AnnouceServiceImpl extends ServiceImpl<AnnounceMapper, Announce> implements AnnounceService {
+
+
+    @Transactional(rollbackFor = Exception.class)
+    @Override
+    public void createAnnounce(Announce announce) {
+        announce.setUsername(SecurityUtils.getUsername());
+        announce.setCreateTime(LocalDateTime.now());
+        announce.setUpdateTime(LocalDateTime.now());
+        announce.setStatus(1);
+        super.save(announce);
+    }
+
+    @Transactional(rollbackFor = Exception.class)
+    @Override
+    public void deleteAnnounce(List<Integer> announceIds) {
+        super.removeByIds(announceIds);
+    }
+
+    @Override
+    public void updateAnnonce(Announce announce) {
+        announce.setUpdateTime(LocalDateTime.now());
+        super.updateById(announce);
+    }
+
+    @Override
+    public PageHelper getAnnounceList(BasicQueryDto basicQueryDto, PageQueryCriteria queryCriteria) {
+        Page<Announce> announcePage = new Page<>(queryCriteria.getPage(), queryCriteria.getSize());
+        Page<Announce> page = super.page(announcePage, wrapper(basicQueryDto));
+        return PageUtils.toPage(page.getRecords(), page.getCurrent(), page.getSize(), page.getTotal());
+    }
+
+    @Override
+    public Announce getOne(Integer announceId) {
+        return super.getById(announceId);
+    }
+
+    private LambdaQueryWrapper<Announce> wrapper(BasicQueryDto basicQueryDto) {
+        LambdaQueryWrapper<Announce> wrapper = Wrappers.<Announce>lambdaQuery();
+        if (ObjectUtils.isNotEmpty(basicQueryDto)) {
+            wrapper.and(StringUtils.isNoneBlank(basicQueryDto.getWord()), q -> {
+                q.or().like(Announce::getTitle, basicQueryDto.getWord());
+                q.or().like(Announce::getUsername, basicQueryDto.getWord());
+            });
+            wrapper.eq(basicQueryDto.getStatus() != 0, Announce::getStatus, basicQueryDto.getStatus());
+            if (StringUtils.isNotBlank(basicQueryDto.getStart()) && StringUtils.isNotBlank(basicQueryDto.getEnd()))
+                wrapper.between(Announce::getCreateTime, DateTimeUtils.dateTime(basicQueryDto.getStart(), DatePattern.NORM_DATETIME_PATTERN), DateTimeUtils.dateTime(basicQueryDto.getEnd(), DatePattern.NORM_DATETIME_PATTERN));
+        }
+        return wrapper;
+    }
 }
